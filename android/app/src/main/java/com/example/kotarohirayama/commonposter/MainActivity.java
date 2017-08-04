4133feb6fdce034b8ca06561f8799fb2c0832444
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-05 07:34:35 +0900

    Implement MainActivity

--- android/app/src/main/java/com/example/kotarohirayama/commonposter/MainActivity.java
+++ android/app/src/main/java/com/example/kotarohirayama/commonposter/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.kotarohirayama.commonposter;
 
+import android.app.Dialog;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.auth0.android.Auth0;
+import com.auth0.android.authentication.AuthenticationException;
+import com.auth0.android.provider.AuthCallback;
+import com.auth0.android.provider.WebAuthProvider;
+import com.auth0.android.result.Credentials;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView token;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        token = (TextView) findViewById(R.id.token);
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+    }
+
+    private void login() {
+        token.setText("Not logged in");
+
+        Auth0 auth0 = new Auth0(this);
+        auth0.setOIDCConformant(true);
+        WebAuthProvider.init(auth0)
+                .withScheme(getString(R.string.auth0_scheme))
+                .start(MainActivity.this, new AuthCallback() {
+                    @Override
+                    public void onFailure(@NonNull final Dialog dialog) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                dialog.show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(final AuthenticationException exception) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(MainActivity.this, "Error: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onSuccess(@NonNull final Credentials credentials) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                token.setText("Logged in: " + credentials.getAccessToken());
+                            }
+                        });
+                    }
+                });
     }
 }
